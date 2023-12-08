10c6fb1c78f7e5d18af2509fff519ee2cdea3625
Noa Kehle <nkehle@calpoly.edu>
2023-12-07 21:32:07 -0800

    working on the TC test class

--- EditDistance.py
+++ EditDistance.py
@@ -6,8 +6,11 @@
 import numpy as np 
 
 class EditDistance:
+    # initialize to empty
     def __init__(self):
-        pass
+        self.matrix = None
+        self.s = None
+        self.t = None
 
     '''
         Overview: Given two string s and t, find the minimum edit distance
@@ -18,26 +21,32 @@ class EditDistance:
             - the minimum distance (int)
     '''
     def findEditDistance(self, s, t):
+        self.s = s
+        self.t = t
         # assign the lengths
         s_length = len(s)
         t_length = len(t)
 
-        arr = np.zeros((s_length, t_length), dtype=int)
+        self.matrix = np.zeros((s_length + 1, t_length + 1), dtype=int)
 
-        # line the empty ones with 0
-        for i in range(s_length):
-            arr[i,0] = 0
-        for j in range(1, t_length):
-            arr[0,j] = 0
+        # init the first rows
+        for i in range(s_length + 1):
+            self.matrix[i, 0] = i
+        for j in range(t_length + 1):
+            self.matrix[0, j] = j
 
-        for i in range(1, s_length):
-            for j in range(1, t_length):
-                if s[i] == t[j]:
-                    arr[i,j] = arr[i-1, j-1]
+        # go through the entire matrix row by row
+        for i in range(1, s_length + 1):
+            for j in range(1, t_length + 1):
+                if s[i - 1] == t[j - 1]:
+                    self.matrix[i, j] = self.matrix[i - 1, j - 1]
                 else:
-                    arr[i,j] = min(arr[i-1, j],arr[i, j-1],arr[i-1, j-1])
-        
-        return arr[i,j]
+                    self.matrix[i, j] = min(self.matrix[i - 1, j],          # deletion
+                                            self.matrix[i, j - 1],          # addition
+                                            self.matrix[i - 1, j - 1]) + 1  # replacement
+
+        # return the bottom right corner
+        return self.matrix[s_length, t_length]
 
     '''
         Overview: Given a solution to editdistance print out the path
@@ -45,5 +54,32 @@ class EditDistance:
         Return:
             - print the path
     '''
-    def recoverAllignment(self):
-        pass
+    def recoverAlignment(self, s, t):
+        i, j = len(s), len(t)
+        path = []
+
+        while i != 0 or j != 0:  # Use 'or' instead of '&'
+            if i > 0 and j > 0 and s[i - 1] == t[j - 1]:
+                path.append(f"Keep character '{s[i - 1]}' @ position {i}")
+                i -= 1
+                j -= 1
+            else:
+                if j == 0 or (i > 0 and self.matrix[i, j - 1] >= self.matrix[i - 1, j]):
+                    path.append(f"Delete character '{s[i - 1]}' @ position {i}")
+                    i -= 1
+                else:
+                    path.append(f"Insert character '{t[j - 1]}' @ position {i}")
+                    j -= 1
+        path.reverse()
+        for move in path:
+            print(move)
+
+
+# driver code
+ed = EditDistance()
+s = "me"
+t = "mee"
+distance = ed.findEditDistance(s, t)
+print("Edit Distance:", distance)
+print("Matrix: \n", ed.matrix)
+ed.recoverAlignment(s, t)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# lab07
+# Noa Kehle
+# nkehle@calpoly.edu
+# CSC3-349-01 -- Fall 2023
+# Lab 07
+
+
